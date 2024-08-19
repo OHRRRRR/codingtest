c345a3f76a075f427ef583d9e33d274a21f2d545
OHRRRRR <63331572+OHRRRRR@users.noreply.github.com>
2024-08-19 17:21:29 +0900

    [Silver II] Title: DFS와 BFS, Time: 136 ms, Memory: 39620 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,44 +1,51 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
+from collections import deque
+sys.setrecursionlimit(10**6)
 
-#정점의 개수, 간성의 개수, 탐색 시작 정점 입력
-n, m, v = map(int,input().split())
-
-node = [[0]*(n+1) for _ in range(n+1)]
-
-visited_dfs = [0]*(n+1)
-visited_bfs = [0]*(n+1)
-
-for _ in range(m):
-  a,b = map(int,input().split())
-  node[a][b]=node[b][a]=1
-
-# 깊이우선 탐색 -> 스택사용, 휴지곽
-# 스택에 넣을 때 프린트해주면 됨
 def dfs(v):
   visited_dfs[v] = 1
-  print(v, end =" ")
+  print(v, end=" ")
+
   for i in range(1,n+1):
-    if not visited_dfs[i] and node[v][i]==1:
+    if not visited_dfs[i] and graph[v][i]==1:
+      visited_dfs[i] = 1
       dfs(i)
 
-#popleft로 가장 먼저 넣은 값부터 출력
 def bfs(v):
-  queue = deque([v])
+  q = deque([v])
+  
   visited_bfs[v]=1
 
-  while queue:
-    v = queue.popleft()
-    print(v, end=" ")
-
+  while q:
+    v = q.popleft()
+    print(v, end = " ")
+    
     for i in range(1,n+1):
-      if not visited_bfs[i] and node[v][i]==1:
+      if not visited_bfs[i] and graph[v][i]==1:
         visited_bfs[i]=1
-        queue.append(i)
+        q.append(i)
+        
 
+
+  
+
+#정점 개수, 간선 개수, 탐색시작정점
+n,m,v = map(int,input().split())
+
+graph = [[0]*(n+1) for _ in range(n+1)]
+
+visited_dfs = [0] * (n+1)
+visited_bfs = [0] * (n+1)
+
+for _ in range(m):
+  x,y = map(int,input().split())
+  
+  graph[x][y]=1
+  graph[y][x]=1
 
 
 dfs(v)
 print()
 bfs(v)
+    
