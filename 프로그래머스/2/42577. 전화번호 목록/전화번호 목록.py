6f15f5ef8325f515118d96064d0c3169d8b52a01
OHRRRRR <63331572+OHRRRRR@users.noreply.github.com>
2025-02-10 13:57:54 +0900

    Update 전화번호 목록.py

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -6,10 +6,10 @@ def solution(phone_book):
     for i in range(len(phone_book)):
         phone[i] = phone_book[i]
     
-    phone_list = sorted(phone.values())  # 전화번호를 정렬하여 사전순으로 정리
+    phone_list = sorted(phone.values())  
 
     for i in range(len(phone_list) - 1):
-        if phone_list[i+1].startswith(phone_list[i]):  # 인접한 번호끼리 접두어 여부 확인
+        if phone_list[i+1].startswith(phone_list[i]): 
             return False
     
     return answer
