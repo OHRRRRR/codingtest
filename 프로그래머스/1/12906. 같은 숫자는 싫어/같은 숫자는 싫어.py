e058593868bce6d05fd32988d206af610efa2146
OHRRRRR <63331572+OHRRRRR@users.noreply.github.com>
2025-02-18 17:21:09 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 104.74 ms, Memory: 27.7 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,6 +1,3 @@
-import sys
-input = sys.stdin.readline
-
 def solution(arr):
     answer = []
     answer.append(arr[0])
@@ -8,36 +5,3 @@ def solution(arr):
         if arr[i]!=arr[i-1]:
             answer.append(arr[i])
     return answer
-
-
-# import sys
-# from collections import deque
-
-# input = sys.stdin.readline
-
-# def solution(arr):
-#     answer = []
-    
-#     # 길이가 1 이상인 경우만 처리
-#     if len(arr) == 0:
-#         return answer
-
-#     for i in range(len(arr) - 1):
-#         if arr[i] == arr[i + 1]:
-#             if i > 0 and arr[i] == arr[i - 1]:
-#                 pass
-#             else:
-#                 answer.append(arr[i])
-#         elif arr.count(arr[i]) == 1:
-#             answer.append(arr[i])
-#         else:
-#             pass
-
-#     # 마지막 요소에 대한 처리
-#     if arr.count(arr[-1]) == 1:
-#         answer.append(arr[-1])
-    
-#     return answer
-
-# arr_input = list(map(int, input().split()))
-# print(solution(arr_input))
